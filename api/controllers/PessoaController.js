ac333fccdd9bff1066747bb06f86860dff931025
Felipe-AR <fefeucn_cm@hotmail.com>
2022-02-06 17:07:28 -0300

    Inclusão dos métodos no controlador de Pessoas

---
+++ api/controllers/PessoaController.js
@@ -0,0 +1,57 @@
+const database = require('../models')
+
+class PessoaController {
+  static async pegaTodasAsPessoas(req, res) {
+    try {
+      const todasAsPessoas = await database.Pessoas.findAll()
+      return res.status(200).json(todasAsPessoas)
+    } catch (erro) {
+      return res.status(500).json(erro.message)
+    }
+  }
+
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params
+    try {
+      const umaPessoa = await database.Pessoas.findOne({ where: { id: Number(id) } })
+      return res.status(200).json(umaPessoa)
+    } catch (erro) {
+      return res.status(500).json(erro.message)
+    }
+  }
+
+  static async criaPessoa(req, res) {
+    const novaPessoa = req.body
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+      return res.status(200).json(novaPessoaCriada)
+    } catch (erro) {
+      return res.status(500).json(erro.message)
+    }
+  }
+
+  static async atualizarPessoa(req, res) {
+    const { id } = req.params
+    const novasInformacoes = req.body
+    try {
+      await database.Pessoas.update(novasInformacoes, { where: { id: Number(id) } })
+      const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id) } })
+      return res.status(200).json(pessoaAtualizada)
+    } catch (erro) {
+      res.status(500).json(erro.message)
+    }
+  }
+
+  static async apagaPessoa(req, res) {
+    const { id } = req.params
+    try {
+      await database.Pessoas.destroy({ where: { id: Number(id) } })
+      return res.status(200).json({mensagem: `O id ${id} foi deletado`})
+    } catch (erro) {
+      res.status(500).json(erro.message)
+    }
+  }
+  const 
+}
+
+module.exports = PessoaController
