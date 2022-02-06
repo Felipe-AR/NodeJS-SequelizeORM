e219a7d1e7ff59a0113f1301a63b06d5392f3fe0
Felipe-AR <fefeucn_cm@hotmail.com>
2022-02-06 17:12:00 -0300

    Responsabilidade de rotas transferida para o arquivo de rotas

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
 
-app.use(bodyParser.json())
-
-app.get('/teste', (req, res) => res
-  .status(200)
-  .send({ mensagem: 'Boas-vindas à API' }))
+routes(app)
 
 app.listen(port, () => console.log(`Servidor está ouvindo na porta ${port}`))
+
+module.exports = app;
