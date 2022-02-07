1a5ef1fadba97c70b23922b92fc512421056de32
Felipe-AR <fefeucn_cm@hotmail.com>
2022-02-07 12:46:51 -0300

    Adiciona a rota de restaurar pessoa e método no controlador

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -52,6 +52,16 @@ class PessoaController {
     }
   }
 
+  static async restauraPessoa(req, res) {
+    const { id } = req.params
+    try {
+      await database.Pessoas.restore({ where: { id: Number(id) } })
+      return res.status(200).json({ mensagem: `O id ${id} foi restaurado` })
+    } catch (erro) {
+      res.status(500).json(erro.message)
+    }
+  }
+
   static async pegaTodasAsMatriculas(req, res) {
     const { estudanteId } = req.params
     try {
