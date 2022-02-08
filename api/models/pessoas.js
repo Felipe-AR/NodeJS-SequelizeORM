bc7c7bc55ded5583f60a76a2d6a303f7adf5035e
Felipe-AR <fefeucn_cm@hotmail.com>
2022-02-08 00:07:50 -0300

    Aplicando o escopo de associação na relação de Matriculas

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -6,13 +6,28 @@ module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
     static associate(models) {
       this.hasMany(models.Turmas, { foreignKey: 'docente_id' })
-      this.hasMany(models.Matriculas, { foreignKey: 'estudante_id' })
+      this.hasMany(models.Matriculas, { foreignKey: 'estudante_id', scope: { status: 'confirmado' }, as: 'aulasMatriculadas' })
     }
   }
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function (dado) {
+          if (dado.length < 3) throw new Error("O campo nome deve possuir mais de 3 caracteres")
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "O dado do tipo e-mail está inválido"
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
