94fe6261810d0c6c64a88b1f12c7a3e4e2a6d833
Felipe-AR <fefeucn_cm@hotmail.com>
2022-02-10 01:36:36 -0300

    Refatoração dó codigo dos controladores para serviços

---
+++ api/services/index.js
@@ -0,0 +1,11 @@
+const PessoasServices = require('./PessoasServices')
+const TurmasServices = require('./TurmasServices')
+const NiveisServices = require('./NiveisServices')
+const MatriculasServices = require('./MatriculasServices')
+
+module.exports = {
+  PessoasServices: PessoasServices,
+  TurmasServices: TurmasServices,
+  NiveisService: NiveisServices,
+  MatriculasServices: MatriculasServices
+}
